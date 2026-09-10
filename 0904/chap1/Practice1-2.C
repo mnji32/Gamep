04adb411f7ace1178df6b1717985e415c0e51e89
mnji32 <bkck2738@naver.com>
2026-09-10 21:45:38 +0900

    Update Practice1-2.C

--- 0904/chap1/Practice1-2.C
+++ 0904/chap1/Practice1-2.C
@@ -11,4 +11,11 @@ printf("%d*%d=%2d",3,i,3*i);
 printf("\n");
 return 0;
 }
+
+void gotoxy(int x, int y)
+{
+COORD Pos = {x - 1, y - 1};
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
 //3단 출력
