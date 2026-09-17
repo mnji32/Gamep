965f02bdab997a911e5e7ac47ada536caa24a523
mnji32 <bkck2738@naver.com>
2026-09-17 21:12:15 +0900

    Update 03example.c

--- 0904(1)/Chap01/화면지우기/03example.c
+++ 0904(1)/Chap01/화면지우기/03example.c
@@ -3,10 +3,16 @@
 
 int main(void)
 {
-char ch;
-printf("문자를 입력하고 Enter>");
-scanf("%c", &ch);
-system("cls");
-printf("입력된 문자 %c\n", ch);
-return 0;
+    char ch;
+
+    printf("문자를 입력하고 Enter>");
+    scanf("%c", &ch);
+    system("cls");
+    // 콘솔 화면 전체를 지워버림 = "문자를 입력하고 Enter>" 부분이 삭제됨
+
+    printf("입력된 문자 %c\n", ch);
+
+    return 0;
 }
+
+// 화면 지우기
