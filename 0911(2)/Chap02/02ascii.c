208ab58d3d2fbb18611e687285f71288e8ce17ee
mnji32 <bkck2738@naver.com>
2026-09-17 22:06:09 +0900

    Update 02ascii.c

--- 0911(2)/Chap02/02ascii.c
+++ 0911(2)/Chap02/02ascii.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <conio.h>
+
 int main(void)
 {
     int chr;
-do
-{
-    chr=getch();
-    if (chr==0 || chr == 0xe0)
-{
-    chr=getch();
-    printf("확장키 code=%d\n", chr);
-}
-else
-    printf("아스키 code=%d\n", chr);
-} while(1);
+
+    do
+    {
+        chr = getch();
+        
+        if (chr == 0 || chr == 0xe0)
+        {
+            chr = getch();
+            printf("확장키 code=%d\n", chr);
+        }
+        else
+        {
+            printf("아스키 code=%d\n", chr);
+        }
+    } while(1);
+
     return 0;
 }
+
+// 방향키 및 일반 키 코드 구분 처리
